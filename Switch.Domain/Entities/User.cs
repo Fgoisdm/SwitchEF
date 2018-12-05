e14a56751bfd20ddf26bc92767b41f72b0a0f0b8
Fagner de Gois <fgois@outlook.com>
2018-12-05 19:11:11 -0200

    Config de contexto e conexão

---
+++ Switch.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Switch.Domain.Enums;
+using System;
+
+
+namespace Switch.Domain.Entities
+{
+    public class User
+    {
+
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string UrlPhoto { get; set; }
+        public GenderEnum Gender { get; set; }
+
+
+    }
+}
---
+++ Switch.Domain/Enums/GenderEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum GenderEnum
+    {
+        Undefined = 0,
+        Female = 1,
+        Male = 2
+    }
+}
